3e4be3264a9b0cabd3ea198d87d3245f24404f34
Jack-w-DOS <jack_w@directonlineservices.co.uk>
2020-04-29 14:24:58 +0100

    Parse date for reviews

--- src/modules/reviews.js
+++ src/modules/reviews.js
@@ -10,6 +10,8 @@ class Reviews {
             head: document.querySelector(locations.head),
             main: document.querySelector(locations.main),
         };
+        this.paginationIndexCount = window.innerWidth < 768 ? 3 : 5;
+        this.paginationShowAllPageSize
         this.init();
     }
     getReviews = () => {
@@ -27,7 +29,7 @@ class Reviews {
                 this.setPagesUI();
             })
             .catch((e) => {
-                // console.error(e);
+                console.error(e);
             });
     };
     getSummary = () => {
@@ -84,12 +86,14 @@ class Reviews {
             const rating = review.products[0].rating.rating;
             const product = review.products[0].product.title;
             const comment = review.products[0].review;
+
+            this.parseDate(date)
             reviewsMarkup += `<div class="review-card card p-4 my-2" data-review="${id}">
                 <a href="${url}">
                     <div class="row-bs">
                         <div class="col-md-3 col-xl-2 d-flex flex-wrap align-content-center">
                             <div class="review-card__name font-weight-6 w-100">${name}</div>
-                            <div class="review-card__time colour-grey3">${date}</div>
+                            <div class="review-card__time colour-grey3">${this.parseDate(date)}</div>
                         </div>
                         <div class="col-md-9 col-xl-10">
                             <div class="review-card__stars">${
@@ -110,6 +114,25 @@ class Reviews {
         }
         this.locations.main.innerHTML = reviewsMarkup;
     };
+    parseDate = (date) => {
+        const currentDate = new Date().getTime()
+        const convertedDate = new Date(date).getTime()
+        const dayDiff = Math.round((currentDate - convertedDate)/(1000*60*60*24))
+        const monthDiff = Math.round((currentDate - convertedDate)/(1000*60*60*24*31))
+        if (dayDiff < 1) {
+            return 'Today'
+        } else if (dayDiff === 1) {
+            return 'Yesterday'
+        } else if (dayDiff > 1 && dayDiff <= 31) {
+            return `${dayDiff} days ago`
+        } else if (monthDiff <= 1){
+            return `a month ago`
+        } else if (monthDiff < 12) {
+            return `${monthDiff} months ago`
+        } else {
+            return 'over a year ago'
+        }
+    }
     setPagesUI = () => {
         const totalPages = this.reviewsData.summary.meta.pages;
         const createEl = (type, classNames, att, content) => {
@@ -150,32 +173,39 @@ class Reviews {
         // Add left control arrow
         pagination.appendChild(leftControl);
         // Are total pages greater than 7
-        const show = 5
-        if (totalPages <= 7) {
+        // TEMP
+        // this.paginationIndexCount = 3
+        if (totalPages <= this.paginationShowAllPageSize) {
             for (let block of indexArray) pagination.appendChild(block);
         } else {
+            const diffMax = Math.floor(this.paginationIndexCount / 2)
             let startDiff = parseInt(this.pageNumber - 1);
             let endDiff = parseInt(totalPages - this.pageNumber)
-            if (startDiff > 2) startDiff = 2
-            if (endDiff > 2) endDiff = 2
-            // Add one and ellipsis if page number is greater than show numbers (5)
-            if (this.pageNumber >=  (show)) {
+            if (startDiff > diffMax) startDiff = diffMax
+            if (endDiff > diffMax) endDiff = diffMax
+            console.log({
+                start: startDiff,
+                end: endDiff,
+                diffMax: diffMax
+            })
+            // Add one and ellipsis if page number is greater than this.paginationIndexCount number
+            if (this.pageNumber - startDiff > 1) {
                 pagination.appendChild(createEl("div","reviews__pages__block reviews__pages__block--index",{ name: "data-index", value: 1 },1))
-                pagination.appendChild(ellipsis.cloneNode(true))
+                if (this.pageNumber - startDiff > 2) pagination.appendChild(ellipsis.cloneNode(true))
             }
-            if (this.pageNumber + show <= totalPages) {
-                for (let i = this.pageNumber - startDiff; i < this.pageNumber + (show - startDiff); i++) {
+            if (this.pageNumber + this.paginationIndexCount <= totalPages) {
+                for (let i = this.pageNumber - startDiff; i < this.pageNumber + (this.paginationIndexCount - startDiff); i++) {
                     // Add 5 numbers with the focus in the middle (is page no > 2)
                     pagination.appendChild(createEl("div","reviews__pages__block reviews__pages__block--index",{ name: "data-index", value: i },i))
                 }
             } else {
-                for (let i = this.pageNumber - (show - endDiff - 1); i <= this.pageNumber + (show - endDiff - 1) && i <= totalPages; i++) {
+                for (let i = this.pageNumber - this.paginationIndexCount + endDiff + 1; i <= this.pageNumber + endDiff && i <= totalPages; i++) {
                     pagination.appendChild(createEl("div","reviews__pages__block reviews__pages__block--index",{ name: "data-index", value: i },i))
                 } 
             }
-            // Add ellipsis and final number if page number is less than final number - show numbers
-            if (this.pageNumber + show - 1 <= totalPages) {
-                pagination.appendChild(ellipsis)
+            // Add ellipsis and final number if page number is less than final number - this.paginationIndexCount numbers
+            if (this.pageNumber + endDiff < totalPages) {
+                if (this.pageNumber + endDiff < totalPages - 1)  pagination.appendChild(ellipsis)
                 pagination.appendChild(createEl("div","reviews__pages__block reviews__pages__block--index",{ name: "data-index", value: totalPages }, totalPages))
             }
         }
@@ -232,9 +262,20 @@ class Reviews {
             this.pagination.control.right.classList.remove('reviews__pages__block--inactive')
         }
     }
+    resizeUpdates = () => {
+        const previousIndex = this.paginationIndexCount
+        if (window.innerWidth < 768) {
+            this.paginationIndexCount = 3
+        } else {
+            this.paginationIndexCount = 5
+        }
+
+        if (previousIndex !== this.paginationIndexCount) this.getReviews()
+    }
     init() {
         this.getSummary();
         this.getReviews();
+        window.addEventListener('resize', this.resizeUpdates)
     }
 }
 
